13f0372de533ae431327e8008232312e27c42337
unknown <svetoslavbaykov55@gmail.com>
2025-03-02 20:27:18 +0200

    Found top 5 heaviest smartphones code updated

--- top_5_heaviest_smartphones/top_5_heaviest_smartphones.py
+++ top_5_heaviest_smartphones/top_5_heaviest_smartphones.py
@@ -1,10 +1,13 @@
 import pandas as pd
-from data.data import df
+
+df = pd.read_csv('../mobiles_dataset_2025.csv')
 
 df["Mobile Weight"] = df["Mobile Weight"].astype(str).str.replace(r"[^\d.]", "", regex=True)
 
 df["Mobile Weight"] = pd.to_numeric(df["Mobile Weight"], errors="coerce")
 
-top_5_heaviest_smartphones = df.nlargest(5, 'Mobile Weight')
+df_clean = df.dropna(subset=['Mobile Weight'])
+
+top_5_heaviest_smartphones = df_clean.nlargest(5, 'Mobile Weight')
 
 print(top_5_heaviest_smartphones[["Model Name", "Company Name", "Mobile Weight"]])
